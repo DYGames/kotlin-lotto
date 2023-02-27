19870e5caef2f38ba446f9affe2913dc2db21dab
Doyeop Kim <dygames34@gmail.com>
2023-02-28 03:46:41 +0900

    refactor(Rank): Rank를 검사할 때 enum의 멤버로 전략을 가지게 수정

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -2,27 +2,23 @@ package lotto.model
 
 import lotto.entity.Money
 
-enum class Rank(val countOfMatch: Int, val winningMoney: Money) {
-    FIRST(6, Money(2_000_000_000)),
-    SECOND(5, Money(30_000_000)),
-    THIRD(5, Money(1_500_000)),
-    FOURTH(4, Money(50_000)),
-    FIFTH(3, Money(5_000)),
-    MISS(0, Money(0));
-
-    private fun determine(countOfMatch: Int, isMatchBonus: Boolean): Boolean {
-        if (this == SECOND && !isMatchBonus)
-            return false
-        if (this == MISS && countOfMatch in (0..2))
-            return true
-        return this.countOfMatch == countOfMatch
-    }
+enum class Rank(
+    val countOfMatch: Int,
+    val winningMoney: Money,
+    val matchStrategy: ((countOfMatch: Int, isMatchBonus: Boolean) -> Boolean)
+) {
+    FIRST(6, Money(2_000_000_000), { countOfMatch, _ -> countOfMatch == 6 }),
+    SECOND(5, Money(30_000_000), { countOfMatch, isMatchBonus -> countOfMatch == 5 && isMatchBonus }),
+    THIRD(5, Money(1_500_000), { countOfMatch, _ -> countOfMatch == 5 }),
+    FOURTH(4, Money(50_000), { countOfMatch, _ -> countOfMatch == 4 }),
+    FIFTH(3, Money(5_000), { countOfMatch, _ -> countOfMatch == 3 }),
+    MISS(0, Money(0), { countOfMatch, _ -> countOfMatch in (0..2) });
 
     companion object {
         private const val ERROR_DETERMINED_RANK_IS_EMPTY = "등수 판별에 실패하였습니다."
 
-        fun determineAll(countOfMatch: Int, isMatchBonus: Boolean): Rank {
-            val determinedRank = Rank.values().find { it.determine(countOfMatch, isMatchBonus) }
+        fun determine(countOfMatch: Int, isMatchBonus: Boolean): Rank {
+            val determinedRank = Rank.values().find { it.matchStrategy(countOfMatch, isMatchBonus) }
             checkNotNull(determinedRank) { ERROR_DETERMINED_RANK_IS_EMPTY }
             return determinedRank
         }
--- src/test/kotlin/lotto/model/RankTest.kt
+++ src/test/kotlin/lotto/model/RankTest.kt
@@ -9,7 +9,7 @@ class RankTest {
     @CsvSource(value = ["6,false,FIRST", "5,true,SECOND", "5,false,THIRD", "4,true,FOURTH", "3,false,FIFTH", "2,true,MISS"])
     @ParameterizedTest
     fun `일치하는 번호 개수와 보너스 번호 일치 여부로 등수 검사`(countOfMatch: Int, isMatchBonus: Boolean, except: Rank) {
-        val actual = Rank.determineAll(countOfMatch, isMatchBonus)
+        val actual = Rank.determine(countOfMatch, isMatchBonus)
 
         assertThat(except).isEqualTo(actual)
     }
